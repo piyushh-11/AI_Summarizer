294036398cd2d131402760e89606735747ec7dd4
Piyush <115842755+piyushh-11@users.noreply.github.com>
2023-12-13 23:30:25 -0600

    store and article

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,19 @@ const Hero = () => {
   return (
     <header className = "w-full flex justify-center items-center flex-col">
         <nav className = "flex justify-between items-center w-full mb-10 pt-3">
-            <img src = { logo } alt = "sumz_logo" className = "w-28 object-contain"/>
+            <img src = { logo } alt = "sumz_logo" className = "w-48 object-contain"/>
 
             <button type = "button" onClick = {() => window.open("https://github.com/piyushh-11/ai_summarizer")} className = "black_btn">
-                Github
+                GitHub
             </button>
         </nav>
 
         <h1 className = "head_text">
-            Summarize Any Article With <b className = "max-md:hidden"/>
-            <span className = "orange_gradient">OpenAI GPT-4</span>
+            Summarize Any Article With <br className = "max-md:hidden"/>
+            <span className = "green_gradient">OpenAI GPT-4</span>
         </h1>
         <h2 className = "desc">
-            Simplify you reading with Sumz, an open source article summarizing tool that transforms long articles into consise summaries
+            Simplify reading with SummaryMe, an open source tool that transforms long articles into consise summaries
         </h2>
     </header>
   )
